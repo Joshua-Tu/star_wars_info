6b172f3b93904c82b49f066e8a01e9372049955a
Joshua Tu <tjh8523@gmail.com>
2019-07-16 04:36:51 +1000

    Film titles shown on the index.html page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from "react";
+import Axios from "axios";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fimls: []
+      loading: false,
+      films: []
     };
+    this.getFilms = this.getFilms.bind(this);
+  }
+
+  getFilms() {
+    return Axios.get("https://swapi.co/api/films").then(res => {
+      this.setState({
+        loading: false
+      });
+      this.setState({ films: res.data.results });
+    });
+  }
+
+  componentDidMount() {
+    this.setState({
+      loading: true
+    });
+    this.getFilms();
   }
 
   render() {
-    return <div className="App" />;
+    const { films, loading } = this.state;
+    console.log(films, loading);
+    const comp = films.map(film => {
+      const title = loading ? "loading..." : film.title;
+      return <p key={film.url}>{title}</p>;
+    });
+
+    return <div className="App">{comp}</div>;
   }
 }
-
 export default App;
