2633aaf6221912a44090e2203b5abcdc2c175853
Joshua Tu <tjh8523@gmail.com>
2019-07-16 05:50:28 +1000

    use fetch method to replace Axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Axios from "axios";
+//import Axios from "axios";
 import FilmList from "./components/FilmList";
 
 class App extends Component {
@@ -9,23 +9,35 @@ class App extends Component {
       loading: false,
       films: []
     };
-    this.getFilms = this.getFilms.bind(this);
+
+    //this.getFilms = this.getFilms.bind(this);
   }
 
-  getFilms() {
-    return Axios.get("https://swapi.co/api/films").then(res => {
-      this.setState({
-        loading: false
-      });
-      this.setState({ films: res.data.results });
-    });
-  }
+  //getFilms() {
+
+  // return Axios.get("https://swapi.co/api/films").then(res => {
+  //   this.setState({
+  //     loading: false,
+  //     films: res.data.results
+  //   });
+  // });
+  //}
 
   componentDidMount() {
     this.setState({
       loading: true
     });
-    this.getFilms();
+
+    //this.getFilms();
+
+    fetch("https://swapi.co/api/films")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          loading: false,
+          films: data.results
+        });
+      });
   }
 
   render() {
