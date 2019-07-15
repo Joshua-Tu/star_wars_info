ed0f2582410b96d9cfad8b80b113783e956faf89
Joshua Tu <tjh8523@gmail.com>
2019-07-16 05:40:22 +1000

    added onClick event handler to the film item.

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -1,19 +1,23 @@
 import React, { Component } from "react";
 
 class FilmList extends Component {
+  handleFilmClick() {
+    console.log("clicked");
+  }
+
   render() {
     const { films, loading } = this.props;
+    console.log(films);
     return (
       <div>
         {films.map(film => {
-          console.log(loading);
           return loading ? (
-            <div key={film.url}>
-              <p>{film.title}</p>
+            <div key={film.url} onClick={this.handleFilmClick}>
+              <h3 className="char-name">Loading...</h3>
             </div>
           ) : (
-            <div key={film.url}>
-              <p>{film.title}</p>
+            <div key={film.url} onClick={this.handleFilmClick}>
+              <h3 className="char-name">{film.title}</h3>
             </div>
           );
         })}
